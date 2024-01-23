ad68619115c5ea984caa06ee91e919930f973d4a
KGoliman <kobegoliman@gmail.com>
2024-01-23 09:24:27 -0800

    task 4

--- quiz1.sh
+++ quiz1.sh
@@ -2,3 +2,9 @@ Name: Kobe Allen Goliman
 Linux username:Kobes-Macbook-Air
 gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "a" | grep -E "^[auotfcm]{4,}$"
 gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "a" | grep -E "^[auotfcm]{4,}$" | wc
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "b" | grep -E "^[bailnrt]{4,}$"
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "b" | grep -E "^[bailnrt]{4,}$" | wc
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "c" | grep -E "^[caonidm]{4,}$"
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "c" | grep -E "^[caonidm]{4,}$" | wc
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "z" | grep -E "^[znorgia]{4,}$"
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "z" | grep -E "^[znorgia]{4,}$" | wc
