f332b07e7aac351cf6bff087605e980d5f5e91c9
KGoliman <kobegoliman@gmail.com>
2024-01-23 09:20:23 -0800

    task 3

--- quiz1.sh
+++ quiz1.sh
@@ -1,2 +1,4 @@
 Name: Kobe Allen Goliman
 Linux username:Kobes-Macbook-Air
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "a" | grep -E "^[auotfcm]{4,}$"
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "a" | grep -E "^[auotfcm]{4,}$" | wc
