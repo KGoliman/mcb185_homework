3261bfdd8e6fd9c13ecc9532212bbb674c613ee1
KGoliman <kobegoliman@gmail.com>
2024-01-27 15:59:38 -0800

    corrected

--- 24accuracy.py
+++ 24accuracy.py
@@ -2,15 +2,15 @@ import math
 import sys
 
 def accuracyf1(tp,tn,fp,fn):
-	if	tp: sys.exit('error: tp cannot be a negative number')
-	elif	tn: sys.exit('error: tn cannot be a negative number')
-	elif	fp: sys.exit('error: fp cannot be a negative number')
-	elif	fn: sys.exit('error: fn cannot be a negative number')
+	if	tp < 0: sys.exit('error: tp cannot be a negative number')
+	elif	tn < 0: sys.exit('error: tn cannot be a negative number')
+	elif	fp < 0: sys.exit('error: fp cannot be a negative number')
+	elif	fn < 0: sys.exit('error: fn cannot be a negative number')
 	else:
 		precision	= tp / (tp + fp)
 		recall 		= tp / (tp + fn)
 		accuracy	= (tp + tn) / (tp + fp + tn + fn)
-		f1			= 2 * ((p * r) / (p + r))
+		f1		= 2 * ((precision * recall) / (precision + recall))
 		return accuracy, f1
 
 print(accuracyf1(4, 5, 6, 7))
