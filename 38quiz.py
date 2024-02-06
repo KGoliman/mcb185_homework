fb8754fd3680a764788fa48ad26ceb00e54b48a6
KGoliman <kobegoliman@gmail.com>
2024-02-06 09:32:57 -0800

    quiz3

--- 38quiz.py
+++ 38quiz.py
@@ -1,4 +1,17 @@
-co-author: Jonathan Raigosa
+# co-author: Jonathan Raigosa
 
-	
+sign_flip = 1
+start = 4
+for i in range(10):
+	gl_denom = (1 + (2 * i))
+	gl = 4 / gl_denom
+	sign_flip = -sign_flip
+	start = start + (sign_flip * gl)
+	print(gl)
+
+for i in range(10):
+	gl = -((-1) ** i) * (4 / (1 + (2 * i)))
+	gl = 4 - gl
+	print(gl)
+
 	
